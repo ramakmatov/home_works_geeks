e53eddcf06a66f38450dc74b61b9e97317de342f
ramilakmatov <79976661+ramakmatov@users.noreply.github.com>
2023-05-27 06:23:51 +0600

    Выполнил 6 задание

---
+++ HW6/script.js
@@ -0,0 +1,33 @@
+    // ДЗ 1
+
+    // const inputField = document.getElementById('inputField');
+    // const reverseButton = document.getElementById('reverseButton');
+    // const output = document.getElementById('output');
+
+    // reverseButton.addEventListener('click', () => {
+    //   const inputValue = inputField.value;
+    //   if (inputValue.trim() !== '') {
+    //     const reversedValue = [...inputValue].reverse().join('');
+    //     output.textContent = reversedValue;
+    //   } else {
+    //     output.textContent = '';
+    //   }
+    // });
+
+    // ДЗ 2 Дороботка
+    
+    // const inputField = document.getElementById('inputField');
+    // const reverseButton = document.getElementById('reverseButton');
+    // const output = document.getElementById('output');
+
+    // reverseButton.addEventListener('click', () => {
+    //   const inputValues = inputField.value.split(',');
+    //   output.innerHTML = '';
+    //   inputValues.forEach(value => {
+    //     if (value.trim() !== '') {
+    //       const listItem = document.createElement('li');
+    //       listItem.textContent = [...value].reverse().join('');
+    //       output.appendChild(listItem);
+    //     }
+    //   });
+    // });
