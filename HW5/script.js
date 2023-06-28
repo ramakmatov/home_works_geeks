928eed9b1752c564160bf701e93d5bd248b02c05
ramilakmatov <79976661+ramakmatov@users.noreply.github.com>
2023-06-28 18:58:20 +0600

    Update script.js

--- HW5/script.js
+++ HW5/script.js
@@ -1,24 +1,34 @@
-    // ДЗ 1
+//Акматов Рамил 
+// ДЗ 1
 
-    // const deleteButtons = document.querySelectorAll('.delete-button');
+var count = 0;
+var countElement = document.getElementById('count');
 
-    // deleteButtons.forEach(button => {
-    //   button.addEventListener('click', () => {
-    //     const project = button.parentNode;
+function updateCounter() {
+  countElement.textContent = count;
+  
+  if (count > 0) {
+    countElement.className = 'counter counter-positive';
+  } else if (count < 0) {
+    countElement.className = 'counter counter-negative';
+  } else {
+    countElement.className = 'counter counter-zero';
+  }
+}
 
-    //     project.remove();
-    //   });
-    // });
+function decrease() {
+  count--;
+  updateCounter();
+}
 
-    // ДЗ 2
-    const editButtons = document.querySelectorAll('.edit-button');
-    editButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        const todo = button.parentNode;
-        const currentText = todo.firstChild.nodeValue.trim();
-        const newText = prompt('Введите новый текст', currentText);
-        if (newText !== null && newText.trim() !== '') {
-          todo.firstChild.nodeValue = newText.trim();
-        }
-      });
-    });
+function reset() {
+  count = 0;
+  updateCounter();
+}
+
+function increase() {
+  count++;
+  updateCounter();
+}
+
+updateCounter();
