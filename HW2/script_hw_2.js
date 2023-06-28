b7762fb2b7d5a836e85db680de6e31e3922f7216
ramilakmatov <79976661+ramakmatov@users.noreply.github.com>
2023-06-28 18:36:39 +0600

    Update script_hw_2.js

--- HW2/script_hw_2.js
+++ HW2/script_hw_2.js
@@ -1,71 +1,121 @@
-// Akmatov Ramil F-26
+// Akmatov Ramil F-27
 //Lesson 2 HW-2
 
-// #1
+// #1 Светофор Вариант с if else
 
-var arrs = [10, 20, 30, 50, 235, 3000, 5000439857];
-var numbers = [];
+var color = prompt("Введите цвет светофора (красный, желтый или зеленый):");
 
-for (var i = 0; i < arrs.length; i++) {
-  var numString = String(arrs[i]);
-  var firstNum = numString.charAt(0);
-
-  if (firstNum === '1' || firstNum === '2' || firstNum === '5') {
-    numbers.push(arrs[i]);
-  }
-}
-
-console.log(numbers);
-
-// #2
-
-var numbers = 20;
-
-while (numbers >= 0){
-    console.log(numbers);
-    numbers--;
-}
-
-// #3
-
-var light = prompt('Введите цвет светофора:', '');
-
-var trafficLight = {
-  red: 'Красный',
-  green: 'Зеленый',
-  yellow: 'Желтый'
-};
-
-if (light === trafficLight.red) {
-  alert('Движение транспорта запрещено!');
-} else if (light === trafficLight.green) {
-  alert('Движение транспорта разрешено!');
-} else if (light === trafficLight.yellow) {
-  alert('Приготовиться к движению!');
+if (color === "красный") {
+  console.log("Стой!");
+} else if (color === "желтый") {
+  console.log("Жди!");
+} else if (color === "зеленый") {
+  console.log("Иди!");
 } else {
-  alert('Ошибка! Попробуйте ввести цвета светофора!');
+  console.log("Некорректный цвет!");
 }
 
-// switch (light) {
-//     case (trafficLight.red):
-//         alert('Движение транспорта запрещено!');
-//         break;
-//     case (trafficLight.green):
-//         alert('Движение транспорта разрешено!');
-//         break;
-//     case (trafficLight.yellow):
-//         alert('Приготовиться к движению!');
-//         break;
-//     default:
-//         alert('Ошибка! Попробуйте ввести цвета светофора!');
-// }
+// #1.2 Светофор Вариант с switch case
 
-// #4
+var color = prompt("Введите цвет светофора (красный, желтый или зеленый):");
 
-var arr = [];
-
-for (var i = 0; i <= 10; i++) {
-    arr.push(i);
-    //console.log(arr)
+switch (color) {
+  case "красный":
+    console.log("Стой!");
+    break;
+  case "желтый":
+    console.log("Жди!");
+    break;
+  case "зеленый":
+    console.log("Иди!");
+    break;
+  default:
+    console.log("Некорректный цвет!");
+    break;
 }
-console.log(arr);
+
+
+// #2 Названии планеты вариант с if else 
+
+var number = parseInt(prompt("Введите порядковый номер планеты:"));
+
+if (number === 1) {
+  console.log("Меркурий");
+} else if (number === 2) {
+  console.log("Венера");
+} else if (number === 3) {
+  console.log("Земля");
+} else if (number === 4) {
+  console.log("Марс");
+} else if (number === 5) {
+  console.log("Юпитер");
+} else if (number === 6) {
+  console.log("Сатурн");
+} else if (number === 7) {
+  console.log("Уран");
+} else if (number === 8) {
+  console.log("Нептун");
+} else if (number === 9) {
+  console.log("Плутон");
+} else {
+  console.log("Некорректный номер планеты!");
+}
+
+
+// #2.2 Названии планеты вариант с switch case
+
+var number = parseInt(prompt("Введите порядковый номер планеты:"));
+
+switch (number) {
+  case 1:
+    console.log("Меркурий");
+    break;
+  case 2:
+    console.log("Венера");
+    break;
+  case 3:
+    console.log("Земля");
+    break;
+  case 4:
+    console.log("Марс");
+    break;
+  case 5:
+    console.log("Юпитер");
+    break;
+  case 6:
+    console.log("Сатурн");
+    break;
+  case 7:
+    console.log("Уран");
+    break;
+  case 8:
+    console.log("Нептун");
+    break;
+  case 9:
+    console.log("Плутон");
+    break;
+  default:
+    console.log("Некорректный номер планеты!");
+    break;
+}
+
+
+// #3 2 числа
+
+var number1 = parseFloat(prompt("Введите первое число:"));
+var number2 = parseFloat(prompt("Введите второе число:"));
+
+if (number1 > number2) {
+  console.log("Первое число больше второго");
+} else if (number1 < number2) {
+  console.log("Второе число больше первого");
+} else {
+  console.log("Числа равны");
+}
+
+// #4 Тернарный оператор
+
+var type = 2;
+var personType = (type === 1) ? "Физическое лицо" : "Юридическое лицо";
+
+console.log(personType);
